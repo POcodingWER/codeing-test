ed717f1c7b709f36ab160f422a1884bdee98f17f
limcuck <89626659+POcodingWER@users.noreply.github.com>
2022-05-23 20:40:03 +0900

    5/23

--- 2단계/124숫자나라.js
+++ 2단계/124숫자나라.js
@@ -1,4 +1,16 @@
 function solution(n) {
     var answer = '';
+    console.log(n.toString(4))
+    console.log(parseInt(n/4))
+    console.log(n%3)
+    console.log(n/3/3/3)
+    console.log(n/3/3/3/3/3/3)
+    
+    for (let i = 1; i <= n; i++) {
+        if(i%4 === 0 ) answer++
+    }
+    console.log(answer*11)
     return answer;
 }
+
+solution()
