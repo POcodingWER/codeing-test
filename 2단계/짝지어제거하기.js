e45cfe16a64db49acd9d804235242a3cb6a316d0
limcuck <89626659+POcodingWER@users.noreply.github.com>
2022-05-25 10:58:13 +0900

    5/25

--- 2단계/짝지어제거하기.js
+++ 2단계/짝지어제거하기.js
@@ -1,8 +1,13 @@
-function solution(s)
-{
+function solution(s){           //스텍구조로 풀면풀림
+    
     var answer = -1;
-    s
-    console.log(s.match(/^[a-z]{2,2}/g))
+    let notPairStack = []
+    for(let i=0; i<s.length; i++){
+        console.log(s[i])
+        notPairStack[notPairStack.length-1] === s[i] ?  notPairStack.pop() : notPairStack.push(s[i])    //같은게있으면 빼고 없으면 넣어주고
+        notPairStack
+    } 
+    answer = notPairStack.length === 0 ? 1 : 0;          //길이가 0이면 성공(1) 아니면 싱패(0)
     return answer;
 }
-solution('baabaa')
+solution('cbaabaac')
