3fdd574b42a2cd678359d7bdb4c018b069cc1f60
limcuck <89626659+POcodingWER@users.noreply.github.com>
2022-05-25 09:26:35 +0900

    5/25

---
+++ 2단계/멀쩡한 사각형.js
@@ -0,0 +1,7 @@
+function solution(w, h) {
+    var answer = 1;
+    
+    return answer;
+}
+
+solution(8,12)
---
+++ 2단계/짝지어제거하기.js
@@ -0,0 +1,8 @@
+function solution(s)
+{
+    var answer = -1;
+    s
+    console.log(s.match(/^[a-z]{2,2}/g))
+    return answer;
+}
+solution('baabaa')
