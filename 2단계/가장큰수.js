6bdcc2a71837feb0937b39858f3368178ad05077
limcuck <89626659+POcodingWER@users.noreply.github.com>
2022-05-28 10:06:05 +0900

    5/28

---
+++ 2단계/가장큰수.js
@@ -0,0 +1,39 @@
+function solution(numbers) {
+    var answer = '';
+    let test
+    numbers.sort().reverse()
+    numbers
+    for (let i = 0; i < numbers.length; i++) {
+        i
+        if(numbers[i]%10===0){
+            i
+            numbers
+            test = numbers.splice(numbers.indexOf(numbers[i]),1);
+            numbers
+            numbers.splice(i+1,0,...test)
+            numbers
+            i++
+        }
+    }
+    numbers
+    return answer;
+}
+
+function solution(numbers) {
+    var answer = '';
+    numbers.sort((a,b)=>sortFunc(a,b))
+    numbers
+    answer = numbers.join('')
+    if(answer[0] === '0') return '0'
+    return answer;
+}
+const sortFunc =  (a,b) =>{
+    const compareA = parseInt(a.toString() + b.toString())
+    compareA
+    const compareB = parseInt(b.toString() + a.toString())
+    compareB
+    console.log(compareB - compareA)
+    return compareB - compareA
+}
+
+solution([3,30,31,33])
